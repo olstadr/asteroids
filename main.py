f25497c14f5c57721b01b90a5cf6314e5df1d787
olstadr <rtolstad23@gmail.com>
2025-07-08 13:12:28 -0400

    initial main & constants

---
+++ main.py
@@ -0,0 +1,27 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+
+    print("""
+Starting Asteroids!
+Screen width: 1280
+Screen height: 720
+""")
+
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+if __name__ == "__main__":
+    main()
